9df463496799daaa2196c6b2e52832d616ce78d7
rjrohan74 <36790079+rjrohan74@users.noreply.github.com>
2018-12-15 01:39:00 +0530

    Add files via upload

---
+++ pythoncode.py
@@ -0,0 +1,28 @@
+import serial 
+import time 
+import pyautogui
+
+ArduinoSerial = serial.Serial('com18',9600) 
+time.sleep(2) 
+
+while 1:
+    incoming = str (ArduinoSerial.readline()) 
+    print incoming
+    
+    if 'Play/Pause' in incoming:
+        pyautogui.typewrite(['space'], 0.2)
+
+    if 'Rewind' in incoming:
+        pyautogui.hotkey('ctrl', 'left')  
+
+    if 'Forward' in incoming:
+        pyautogui.hotkey('ctrl', 'right') 
+
+    if 'Vup' in incoming:
+        pyautogui.hotkey('ctrl', 'down')
+        
+
+    if 'Vdown' in incoming:
+        pyautogui.hotkey('ctrl', 'up')
+
+    incoming = "";
